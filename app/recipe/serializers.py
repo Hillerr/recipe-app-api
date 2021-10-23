dcd49fb7512600c792d79d9785c0c9d500b407c4
Hillerr <rafaelhiller23@gmail.com>
2021-10-23 19:20:13 -0300

    Added create recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -40,3 +40,9 @@ class RecipeSerializer(ModelSerializer):
             'time_minutes', 'price', 'link'
         )
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serilize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
